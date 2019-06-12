e8375f03fc0c09e9c760e957c306f73b7c187824
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-11 22:27:04 -0500

    Added knex/migration created schema for authiii db users table

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const bcrypt = require('bcryptjs')
+const knex = require('knex')
+
 const server = express()
 
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+const config ={
+    client:"sqlite3",
+    connection: {
+        filename:'./data/authiii.db3'
+    },
+    useNullasDefault: true
+};
+
+const db = knex(config)
+
+
+
+
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~SERVER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const port = process.env.PORT || 6492
 server.listen(port, () => {
     console.log(`\n We're live in port ${port}! \n`)
