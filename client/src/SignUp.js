d1aba775031e69f9bccb3af3b89af9aee3e3a9cd
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-15 00:25:32 -0500

    created client react app

---
+++ client/src/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SignUp extends React.Component{
+    render() {
+        return (
+            <div>
+                <h3>SignUp</h3>
+            </div>
+        )
+    }
+}
+
+export default SignUp
