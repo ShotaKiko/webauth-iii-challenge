2907ea08066a576f0ff3e01b800caac9a195e805
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-16 01:04:34 -0500

    added routes and nav links to diff pages/added minimal nav styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Route, NavLink } from 'react-router-dom'
+import Login from './Login.js'
+import SignUp from './SignUp.js'
+import Users from './Users.js'
 
 function App() {
   return (
     <div className="App">
-      hi
+      <h1>Welcome to Web Auth III</h1>
+        <nav>
+          <NavLink to = '/'>Home</NavLink>
+          <NavLink to = '/login'>Login</NavLink>
+          <NavLink to = '/signup'>Sign Up</NavLink>
+          <NavLink to = '/users'>User List</NavLink>
+        </nav>
+        
+        <div>
+          <Route path = '/login' component = {Login} />
+          <Route path = '/signup' component = {SignUp} />
+          <Route path = '/users' component = {Users} />
+        </div>
     </div>
   );
 }
